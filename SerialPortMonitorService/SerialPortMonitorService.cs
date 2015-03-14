d63320c888f38609577f27518a0880f8b9b8b8b9
netmf-labs <no@rep.ly>
2015-03-14 10:57:42 +0300

    initial commit

---
+++ SerialPortMonitorService/SerialPortMonitorService.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.ServiceProcess;
+using System.Text;
+
+namespace SerialPortMonitorService
+{
+    public enum ServiceState
+    {
+        SERVICE_STOPPED = 0x00000001,
+        SERVICE_START_PENDING = 0x00000002,
+        SERVICE_STOP_PENDING = 0x00000003,
+        SERVICE_RUNNING = 0x00000004,
+        SERVICE_CONTINUE_PENDING = 0x00000005,
+        SERVICE_PAUSE_PENDING = 0x00000006,
+        SERVICE_PAUSED = 0x00000007,
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct ServiceStatus
+    {
+        public long dwServiceType;
+        public ServiceState dwCurrentState;
+        public long dwControlsAccepted;
+        public long dwWin32ExitCode;
+        public long dwServiceSpecificExitCode;
+        public long dwCheckPoint;
+        public long dwWaitHint;
+    };
+
+    public partial class SerialPortMonitorService : ServiceBase
+    {
+        [DllImport("advapi32.dll", SetLastError = true)]
+        private static extern bool SetServiceStatus(IntPtr handle, ref ServiceStatus serviceStatus);
+        private ServiceStatus _serviceStatus;
+        private System.Diagnostics.EventLog eventLog;
+        private int eventId;
+
+        public SerialPortMonitorService(string[] args)
+        {
+            InitializeComponent();
+
+            String arr = args[0];
+
+            eventLog = new System.Diagnostics.EventLog();
+            if (!System.Diagnostics.EventLog.SourceExists("MySource"))
+            {
+                System.Diagnostics.EventLog.CreateEventSource("MySource", "MyNewLog");
+            }
+            eventLog.Source = "MySource";
+            eventLog.Log = "MyNewLog";
+
+            eventLog.WriteEntry("Starting with args: " + arr);
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            _serviceStatus = new ServiceStatus();
+            SetStartPendingStatus(_serviceStatus);
+
+            eventLog.WriteEntry("In OnStart");
+
+            // Set up a timer to trigger every minute.
+            System.Timers.Timer timer = new System.Timers.Timer();
+            timer.Interval = 6000; // 60 seconds
+            timer.Elapsed += new System.Timers.ElapsedEventHandler(this.OnTimer);
+            timer.Start();
+
+            SetRunningStatus(_serviceStatus);
+        }
+
+        private void DoAll()
+        {
+            string connectionString = "Data Source=(local);Initial Catalog=SensorsDB;" + "Integrated Security=true";
+
+            // Provide the query string with a parameter placeholder.
+            string queryString =
+                "select top @number from * Temperatures;";
+
+            // Specify the parameter value.
+            int paramValue = 5;
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                // Create the Command and Parameter objects.
+                SqlCommand command = new SqlCommand(queryString, connection);
+                command.Parameters.AddWithValue("@number", paramValue);
+
+                try
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        eventLog.WriteEntry("Fuck: " + reader[0] /*+ ", " + reader[1] + ", " + reader[2]*/);
+                    }
+                    reader.Close();
+                }
+                catch (Exception ex)
+                {
+                    eventLog.WriteEntry("Failed to access database: " + ex.Message);
+                }
+            }
+        }
+
+        private void SetRunningStatus(ServiceStatus serviceStatus)
+        {
+            // Update the service state to Running.
+            serviceStatus.dwCurrentState = ServiceState.SERVICE_RUNNING;
+            SetServiceStatus(this.ServiceHandle, ref serviceStatus);
+        }
+
+        private void SetStartPendingStatus(ServiceStatus serviceStatus)
+        {
+            // Update the service state to Start Pending.            
+            serviceStatus.dwCurrentState = ServiceState.SERVICE_START_PENDING;
+            serviceStatus.dwWaitHint = 100000;
+            SetServiceStatus(this.ServiceHandle, ref serviceStatus);
+        }
+
+        protected override void OnStop()
+        {
+            eventLog.WriteEntry("In OnStop");
+        }
+
+        public void OnTimer(object sender, System.Timers.ElapsedEventArgs args)
+        {
+            // TODO: Insert monitoring activities here.
+            DoAll();
+            eventLog.WriteEntry("Monitoring the System", EventLogEntryType.Information, eventId++);
+        }
+
+
+    }
+}
---
+++ SerialPortMonitorService/ServiceInstaller.Designer.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+
+namespace SerialPortMonitorService
+{
+    partial class ServiceInstaller
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void OnBeforeInstall(IDictionary savedState)
+        {
+            string parameter = "MySource1\" \"MyLogFile1";
+            Context.Parameters["assemblypath"] = "\"" + Context.Parameters["assemblypath"] + "\" \"" + parameter + "\"";
+            base.OnBeforeInstall(savedState);
+        }
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.serviceProcessInstaller1 = new System.ServiceProcess.ServiceProcessInstaller();
+            this.serviceInstaller1 = new System.ServiceProcess.ServiceInstaller();
+            // 
+            // serviceProcessInstaller1
+            // 
+            this.serviceProcessInstaller1.Account = System.ServiceProcess.ServiceAccount.LocalSystem;
+            this.serviceProcessInstaller1.Password = null;
+            this.serviceProcessInstaller1.Username = null;
+            this.serviceProcessInstaller1.AfterInstall += new System.Configuration.Install.InstallEventHandler(this.serviceProcessInstaller1_AfterInstall);
+            // 
+            // serviceInstaller1
+            // 
+            this.serviceInstaller1.Description = "Service for monitoring serial port and writing aquired data into SQL database.";
+            this.serviceInstaller1.DisplayName = "Serial Port Monitor Service";
+            this.serviceInstaller1.ServiceName = "SerialPortMonitorService";
+            this.serviceInstaller1.StartType = System.ServiceProcess.ServiceStartMode.Automatic;
+            // 
+            // ServiceInstaller
+            // 
+            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+            this.serviceProcessInstaller1,
+            this.serviceInstaller1});
+
+        }
+
+        #endregion
+
+        private System.ServiceProcess.ServiceProcessInstaller serviceProcessInstaller1;
+        private System.ServiceProcess.ServiceInstaller serviceInstaller1;
+    }
+}
